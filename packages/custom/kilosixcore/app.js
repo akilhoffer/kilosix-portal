c49377d4393c8d3a85fb35660f6411ced6213347
Anthony Kilhoffer <anthony@kilhoffer.com>
2015-11-16 08:49:08 -0700

    Added mongodb connection strings

--- packages/custom/kilosixcore/app.js
+++ packages/custom/kilosixcore/app.js
@@ -1,52 +1,46 @@
 'use strict';
 
-/*
- * Defining the Package
- */
 var Module = require('meanio').Module;
-
 var Kilosixcore = new Module('kilosixcore');
 
-/*
- * All MEAN packages require registration
- * Dependency injection is used to define required modules
- */
-Kilosixcore.register(function(app, auth, database) {
+Kilosixcore.register(function (app, auth, database, system) {
+    // NOTE: DO NOT remove the 'system' argument above. It's here to force the system package to load before this module
+    //       and allow us to override the default template.
 
-  //We enable routing. By default the Package Object is passed to the routes
-  Kilosixcore.routes(app, auth, database);
+    //We enable routing. By default the Package Object is passed to the routes
+    Kilosixcore.routes(app, auth, database);
 
-  //We are adding a link to the main menu for all authenticated users
-  Kilosixcore.menus.add({
-    title: 'kilosixcore example page',
-    link: 'kilosixcore example page',
-    roles: ['authenticated'],
-    menu: 'main'
-  });
-  
-  Kilosixcore.aggregateAsset('css', 'kilosixcore.css');
+    //We are adding a link to the main menu for all authenticated users
+    //Kilosixcore.menus.add({
+    //    title: 'kilosixcore example page',
+    //    link: 'kilosixcore example page',
+    //    roles: ['authenticated'],
+    //    menu: 'main'
+    //});
 
-  /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    Kilosixcore.settings({
+    Kilosixcore.aggregateAsset('css', 'kilosixcore.css');
+
+    /**
+     //Uncomment to use. Requires meanio@0.3.7 or above
+     // Save settings with callback
+     // Use this for saving data from administration pages
+     Kilosixcore.settings({
         'someSetting': 'some value'
     }, function(err, settings) {
         //you now have the settings object
     });
 
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    Kilosixcore.settings({
+     // Another save settings example this time with no callback
+     // This writes over the last settings.
+     Kilosixcore.settings({
         'anotherSettings': 'some value'
     });
 
-    // Get settings. Retrieves latest saved settigns
-    Kilosixcore.settings(function(err, settings) {
+     // Get settings. Retrieves latest saved settigns
+     Kilosixcore.settings(function(err, settings) {
         //you now have the settings object
     });
-    */
+     */
 
-  return Kilosixcore;
+    return Kilosixcore;
 });
